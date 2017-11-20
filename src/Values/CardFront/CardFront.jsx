c16f370f031f2a885c45b04bd46eb98580537ff0
Tuomas Peippo <tuomas@peippo.eu>
2017-11-20 10:37:11 +0200

    Update card styles

--- src/Values/CardFront/CardFront.jsx
+++ src/Values/CardFront/CardFront.jsx
@@ -4,13 +4,14 @@ import React from 'react';
 const classes = type => `jp-card-logo jp-card-${type}`;
 
 const CardFront = props => (
-  <div className="jp-card-front" >
-    <div className={classes(props.type)} />
-    <div className="jp-card-lower" >
+  <div className="jp-card-front">
+    <div className={classes(props.type)}>{props.type}</div>
+    <div className="jp-card-lower">
       <div className="jp-card-shiny" />
       {props.children}
     </div>
-  </div>);
+  </div>
+);
 
 CardFront.propTypes = {
   children: PropTypes.node.isRequired,
